5b846ff2f4785a2e9f63ccadc30cad61b30e5995
Dominik Meyer <dominik.meyer.95@outlook.com>
2022-11-02 00:38:35 +0100

    linked modules per asset class to asset_pricing subpackage

---
+++ quant_toolkit/asset_pricing/__init__.py
@@ -0,0 +1,7 @@
+from quant_toolkit.asset_pricing import equities
+from quant_toolkit.asset_pricing import rates
+from quant_toolkit.asset_pricing import credits
+from quant_toolkit.asset_pricing import forwards
+from quant_toolkit.asset_pricing import futures
+from quant_toolkit.asset_pricing import swaps
+from quant_toolkit.asset_pricing import options
